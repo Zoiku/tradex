313fd1907aab397d643eea15284c78b12bf30674
“mark <mark.zoiku@ashesi.edu.gh>
2021-07-23 23:17:09 +0000

    Updated DB connection

--- Database-Connection/connection.php
+++ Database-Connection/connection.php
@@ -1,11 +1,11 @@
 <?php 
-    // error_reporting(0);    
+    error_reporting(0);    
 
     // Variables to connect to database
-    $servername = 'localhost';
-    $username = 'root';
-    $password = getenv('MYSQL_ROOT') ?? '';
-    $dbname = 'myproject';
+    $servername = 'us-cdbr-east-04.cleardb.com';
+    $username = 'b088a86b970f6f';
+    $password = "97bec10a";
+    $dbname = 'heroku_36a0a1780e5576c';
 
     //Proceedural approach to connection to database
     $conn = mysqli_connect($servername, $username, $password, $dbname);
