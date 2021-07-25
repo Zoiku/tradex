f6e68d5675159e54169f87c4aba9cc52f7698fd0
“mark <mark.zoiku@ashesi.edu.gh>
2021-07-25 13:44:39 +0000

    Page Unavailable

--- pageunavailable.php
+++ pageunavailable.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="./Css/style.css">
-    <link rel="icon" href="./Assets/favicon_io/favicon.ico">
-</head>
-<body>
-    <div class="pageunavailable">
-        <small>Mobile version in progress, please view on a desktop or laptop...
-    </small>
-    </div>    
-</body>
-</html>
+<?php 
+  echo <<<_String
+        <div class="pageunavailable">
+            <small>Mobile version in progress, please view on a desktop or laptop...</small>
+        </div> 
+    _String;
+?>
